1853ccad36f4d2dc76f180bfecbbb05995c3114f
puhrome <phirom.yim@fullsail.edu>
2014-05-29 23:14:23 -0400

    Personal Functions
    personal functions for assignment

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Personal Functions Created by puhrome on 5/29/14.
+ */
+var total = tvTime;
+
+function tvTime = (7,(prompt("How many hours of Tv do you watch a day?"))){
+    var weeklyTv = 7 * (prompt("How many hours of Tv do you watch a day?"));
+    alert("Please enter a valid number");
+    return weeklyTv;
+}
+console.log(total);
+
+
+
+var
